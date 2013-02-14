0306da3b9efeca4b8d557e517c16521c4868326e
Ming Chow <mchow@cs.tufts.edu>
2013-02-14 00:25:59 -0500

    Added gesture examples

---
+++ gestures/TouchDragPan/TouchDragPan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchDragPan
+//
+//  Created by Ming Chow on 1/18/12.
+//  Copyright (c) 2012 Tufts University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
